38622cac26710ca6d8d9c53dcdaf096ff4e767e3
lee <840591418@qq.com>
2019-09-11 21:30:41 +0800

    测试代码提交2

---
+++ MongoDB_Demo01/src/test/java/com/lch/Test07.java
@@ -0,0 +1,15 @@
+package com.lch;
+
+/**
+ * @ClassName:Test07
+ * @Author：Mr.lee
+ * @DATE：2019/09/11
+ * @TIME： 21:29
+ * @Description: TODO
+ */
+public class Test07 {
+
+    public static void main(String[] args) {
+        System.out.println("传奇好牛逼啊！！！");
+    }
+}
