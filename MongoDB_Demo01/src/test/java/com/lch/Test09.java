88f56d7b5e23801a7841386c8a97a5e2974a46ff
lee <840591418@qq.com>
2019-09-11 21:49:35 +0800

    创建分支的测试03

---
+++ MongoDB_Demo01/src/test/java/com/lch/Test09.java
@@ -0,0 +1,15 @@
+package com.lch;
+
+/**
+ * @ClassName:Test09
+ * @Author：Mr.lee
+ * @DATE：2019/09/11
+ * @TIME： 21:48
+ * @Description: TODO
+ */
+public class Test09 {
+
+    public static void main(String[] args) {
+        System.out.println("创建分支的测试！！！");
+    }
+}
