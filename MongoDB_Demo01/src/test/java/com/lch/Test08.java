08ad76506c49518a139dacc061344066f2e0122c
lee <840591418@qq.com>
2019-09-11 21:37:03 +0800

    测试代码提交2

---
+++ MongoDB_Demo01/src/test/java/com/lch/Test08.java
@@ -0,0 +1,15 @@
+package com.lch;
+
+/**
+ * @ClassName:Test08
+ * @Author：Mr.lee
+ * @DATE：2019/09/11
+ * @TIME： 21:34
+ * @Description: TODO
+ */
+public class Test08 {
+
+    public static void main(String[] args) {
+        System.out.println("404架构鸟1！！");
+    }
+}
